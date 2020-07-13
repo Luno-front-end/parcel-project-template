f90b1dbf6181b0a03ebbd9fd875ea85922c8ca06
Maksym Zaika <Onlyrobochaposhta@gmail.com>
2020-07-14 01:52:44 +0300

    Gooooood slide 😎😎😎

---
+++ src/js/slider-line.js
@@ -0,0 +1,38 @@
+// const slidersToShow = 1;
+// const container = document.querySelector('.overlay');
+// const track = document.querySelector('.slider-container');
+// const items = document.querySelectorAll('.slider-line');
+
+// const itemWidth = container.clientWidth / slidersToShow;
+
+// items.forEach(item => {
+//   item.style.minWidth = `${itemWidth}px`;
+// });
+
+// var controls = document.querySelectorAll('.controls');
+// for (var i = 0; i < controls.length; i++) {
+//   controls[i];
+// }
+
+// var slides = document.querySelectorAll('.slider-container .slider-line');
+// var currentSlide = 0;
+// var slideInterval = setInterval(nextSlide, 5000);
+
+// function nextSlide() {
+//   goToSlide(currentSlide + 1);
+// }
+
+// function prevSlide() {
+//   goToSlide(currentSlide - 1);
+// }
+
+// function goToSlide(n) {
+//   slides[currentSlide].className = 'slider-line';
+//   currentSlide = (n + slides.length) % slides.length;
+//   slides[currentSlide].className = 'slider-line showing';
+// }
+
+// function pauseSlideshow() {
+//   playing = false;
+//   clearInterval(slideInterval);
+// }
