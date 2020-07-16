b6d8d2b8b462f765993af92bff292410efa1308f
Maksym Zaika <Onlyrobochaposhta@gmail.com>
2020-07-17 00:50:46 +0300

    Updates

--- menu.0c91648c.js
+++ menu.0c91648c.js
@@ -120,14 +120,12 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 })({"js/menu.js":[function(require,module,exports) {
 (function () {
   var menuBtnRef = document.querySelector('[data-menu-button]');
-  var mobileMenuRef = document.querySelector('[data-menu]'); // const mobileMenuSroll = document.querySelector('[menu-container]');
-
+  var mobileMenuRef = document.querySelector('[data-menu]');
   menuBtnRef.addEventListener('click', function () {
     var expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
-    document.header.classList.toggle('menu-open');
   });
 })();
 
@@ -167,7 +165,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59669" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55788" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
